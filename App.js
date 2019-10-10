9ed72deca6267994ffe0d7b3c2b7eff058e7feec
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-10-10 16:05:36 +0200

    Added App05

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import App01 from './Apps/App01'
 import App02 from './Apps/App02'
 import App03 from './Apps/App03'
 import App04 from './Apps/App04'
+import App05 from './Apps/App05'
 
 export default function App() {
-	return <App04 />
+	return <App05 />
 }
---
+++ Apps/App05.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+class App04 extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+
+		this.colors = ['red', 'green', 'blue', 'cyan', 'magenta', 'yellow']
+	}
+
+	render() {
+		return (
+			<View style={styles.wrapper}>
+				<View style={styles.container}>
+					{this.colors.map((color, i) => (
+						<Item key={i} flex={1} text={`Item ${i}`} backgroundColor={color} color={'white'} />
+					))}
+				</View>
+				<View style={[styles.container, { flexDirection: 'column-reverse' }]}>
+					{this.colors.map((color, i) => (
+						<Item key={i} flex={1} text={`Item ${i}`} backgroundColor={color} color={'black'} />
+					))}
+				</View>
+			</View>
+		)
+	}
+}
+
+class Item extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+
+	render() {
+		return (
+			<View style={{ ...this.props }}>
+				<Text style={[styles.text, { color: this.props.color }]}> {this.props.text} </Text>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	wrapper: {
+		flex: 1,
+		flexDirection: 'row',
+	},
+	container: {
+		flex: 1,
+		backgroundColor: '#ffff00',
+	},
+	text: {
+		fontSize: 48,
+	},
+})
+
+export default App04
