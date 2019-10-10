9e3603ce6a920c01587c90e25ad04a4b28420529
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-10-10 16:00:06 +0200

    Added App04

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { StyleSheet, Text, View } from 'react-native'
 import App01 from './Apps/App01'
 import App02 from './Apps/App02'
 import App03 from './Apps/App03'
+import App04 from './Apps/App04'
 
 export default function App() {
-	return <App03 />
+	return <App04 />
 }
